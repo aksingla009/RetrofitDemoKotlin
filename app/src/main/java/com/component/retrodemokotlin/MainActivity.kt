24e281549c7e0581adfb7dd3db1f4b536d8556ad
Amandeep Singla <aksingla009@gmail.com>
2020-06-03 21:50:49 +0530

    Using Path Parameters and restructuring the code

--- app/src/main/java/com/component/retrodemokotlin/MainActivity.kt
+++ app/src/main/java/com/component/retrodemokotlin/MainActivity.kt
@@ -13,15 +13,26 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private val TAG : String = "MainActivity"
+    private lateinit var retrofitService : AlbumService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retrofitService  = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        retrofitService  = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
 
+        getRequest()
+
+        //getRequestWithQueryParameters()
+        //getRequestWithPathParameters()
+
+
+
+
+    }
+
+    private fun getRequest() {
         val responseLiveData : LiveData<Response<Albums>> = liveData {
-            val response = retrofitService.getSortedAlbums(3)
-
+            val response = retrofitService.getAlbums()
             emit(response)
         }
 
@@ -38,6 +49,43 @@ class MainActivity : AppCompatActivity() {
                     textView.append(result)
 
                 }
+            }
+        })
+
+    }
+
+    private fun getRequestWithQueryParameters() {
+        val responseLiveData : LiveData<Response<Albums>> = liveData {
+            val response = retrofitService.getSortedAlbums(3)
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if(albumsList != null){
+                while(albumsList.hasNext()){
+                    val albumsItem = albumsList.next()
+
+                    val result : String = " " + "Album Title : ${albumsItem.title}" + "\n" +
+                            " " + "Album ID : ${albumsItem.id}" + "\n" +
+                            " " + "User ID : ${albumsItem.userId}" + "\n\n\n"
+
+                    textView.append(result)
+
+                }
+            }
+        })
+    }
+
+    private fun getRequestWithPathParameters() {
+        val responseItem : LiveData<Response<AlbumsItem>> = liveData {
+            val response = retrofitService.getParticularAlbumWithAlbumID(3)
+            emit(response)
+        }
+
+        responseItem.observe(this, Observer {
+            it.body()?.let {
+                Log.i(TAG,it.title)
             }
         })
     }
--- app/src/main/java/com/component/retrodemokotlin/network/AlbumService.kt
+++ app/src/main/java/com/component/retrodemokotlin/network/AlbumService.kt
@@ -1,8 +1,10 @@
 package com.component.retrodemokotlin.network
 
 import com.component.retrodemokotlin.Albums
+import com.component.retrodemokotlin.AlbumsItem
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface AlbumService {
@@ -10,6 +12,11 @@ interface AlbumService {
     @GET("/albums")
     suspend fun getAlbums() : Response<Albums>
 
+    //Query Parameter
     @GET("/albums")
     suspend fun getSortedAlbums(@Query("userId") passedUserID : Int) : Response<Albums>
+
+    //Path Parameter
+    @GET("/albums/{id}")
+    suspend fun getParticularAlbumWithAlbumID (@Path(value = "id") albumID : Int) : Response<AlbumsItem>
 }
